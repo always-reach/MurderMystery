78ff96401cd49e0e9317b0cff11e36227bf65a39
always-reach <67514209+always-reach@users.noreply.github.com>
2022-09-29 23:07:45 +0900

    first commit

---
+++ front/pages/login.tsx
@@ -0,0 +1,11 @@
+import { EmailField } from "../components/TextForm"
+
+function Login() {
+    return (
+        <div>
+            <h1 className="text-3xl font-bold underline">Login Pageだよ</h1>
+            <EmailField />
+        </div>)
+}
+
+export default Login
