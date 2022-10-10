0f9d0cedd39a107450bb10bd326397d1cc92ee17
always-reach <67514209+always-reach@users.noreply.github.com>
2022-10-10 18:36:10 +0900

    fix layout

--- front/components/inputForm/PasswordForm.tsx
+++ front/components/inputForm/PasswordForm.tsx
@@ -1,17 +1,30 @@
 import { forwardRef } from "react"
+import ErrorMessage from "../ErrorMessage"
 
 type InputProps = JSX.IntrinsicElements["input"]
+type PasswordProps = InputProps & { error?: boolean, errorMessage?: string }
 
-
-const PasswordForm = forwardRef<HTMLInputElement, InputProps>((props, ref) => {
+const PasswordForm = forwardRef<HTMLInputElement, PasswordProps>((props, ref) => {
+    const { error, errorMessage, ...others } = props
+    const defaultStyle = "border text-sm rounded-lg block w-full p-2.5 "
+    const colorStyle = "bg-gray-50 border-gray-300 text-gray-900 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+    const errorStyle = "bg-red-50 border-red-500 text-red-900 placeholder-red-700 focus:ring-red-500 focus:border-red-500 dark:text-red-500 dark:placeholder-red-500 dark:border-red-500"
+    let PasswordStyle = ""
+    if (error) {
+        PasswordStyle = `${defaultStyle} ${errorStyle}`
+    } else {
+        PasswordStyle += `${defaultStyle} ${colorStyle}`
+    }
+    
     return (
         <div className="w-full mb-6">
             <input type="password"
-                {...props}
+                {...others}
                 autoComplete="current-password"
-                className="mx-auto bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-7/12 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                className={PasswordStyle}
                 placeholder="パスワード"
                 ref={ref} />
+            {error && <ErrorMessage >{errorMessage}</ErrorMessage>}
         </div>
 
     )
--- front/pages/login.tsx
+++ front/pages/login.tsx
@@ -1,7 +1,9 @@
 import { useMutation } from "@apollo/client"
 import { useRouter } from "next/router"
 import * as React from "react"
+import * as yup from 'yup'
 import { SubmitHandler, useForm } from "react-hook-form"
+import { yupResolver } from '@hookform/resolvers/yup'
 
 import PrimaryButton from "../components/button/primaryButton"
 import Divider from "../components/Divider"
@@ -10,22 +12,27 @@ import HyperLink from "../components/HyperLink"
 import CheckBoxForm from "../components/inputForm/CheckBocForm"
 import EmailForm from "../components/inputForm/EmailForm"
 import PasswordForm from "../components/inputForm/PasswordForm"
-import { LOGIN_USER,MutationUser } from "../graphql/mutation/User.mutation"
+import { LOGIN_USER, MutationUser } from "../graphql/mutation/User.mutation"
 
 type LoginInput = {
     email: string
     password: string
 }
+const validateSchema = yup.object().shape({
+    email: yup.string().email("メールアドレスの形式が違います").required("必須入力です"),
+    password: yup.string().required("必須入力です").min(8, "パスワードは８文字以上です")
+})
 
 function Login() {
-    const [login, { data, error }] = useMutation<MutationUser>(LOGIN_USER)
-    const { register, handleSubmit } = useForm<LoginInput>()
+    const [login, { data }] = useMutation<MutationUser>(LOGIN_USER)
+    const { register, handleSubmit, formState: { errors } } = useForm<LoginInput>({ mode: "onSubmit", resolver: yupResolver(validateSchema) })
     const router = useRouter()
 
     const onSubmit: SubmitHandler<LoginInput> = async (loginInput) => {
         console.log(loginInput.email)
-        const response=await login({ variables: { email: loginInput.email, password: loginInput.password } })
-        if(response.data?.loginUser.user.id){
+        await login({ variables: { email: loginInput.email, password: loginInput.password } })
+        console.log(data)
+        if (data) {
             router.push("/top")
         }
 
@@ -36,10 +43,12 @@ function Login() {
             <div className="h-screen w-screen flex justify-center">
                 <div className="bg-gray-100 border-2 border-gray-900 rounded-lg w-5/12 mt-20 mb-auto py-12">
                     <form onSubmit={handleSubmit(onSubmit)}>
-                        <EmailForm {...register("email")} />
-                        <PasswordForm {...register("password")} />
-                        <CheckBoxForm id="checkbox" label="ログイン状態を保持する" />
-                        <PrimaryButton type="submit" label="ログインする" />
+                        <div className="mx-auto w-7/12">
+                            <EmailForm {...register("email", { required: true })} error={"email" in errors} errorMessage={errors.email?.message} />
+                            <PasswordForm {...register("password", { required: true })} error={"password" in errors} errorMessage={errors.password?.message} />
+                            <CheckBoxForm id="checkbox" label="ログイン状態を保持する" />
+                            <PrimaryButton type="submit" label="ログインする" onClick={() => { console.log(errors) }} />
+                        </div>
                         <Divider />
                         <div className="flex justify-evenly">
                             <HyperLink href="/signon">新規登録の方はこちら</HyperLink>
