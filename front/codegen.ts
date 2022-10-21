75dab43a535e5f37a4a89504141e5594230f817d
always-reach <67514209+always-reach@users.noreply.github.com>
2022-10-22 00:25:22 +0900

    first codegen

--- front/codegen.ts
+++ front/codegen.ts
@@ -3,16 +3,13 @@ import type { CodegenConfig } from '@graphql-codegen/cli';
 
 const config: CodegenConfig = {
   overwrite: true,
-  schema: "http://localhost:8000/graphql",
-  documents: "src/**/*.tsx",
+  schema: "./schema.json",
+  documents: ["./graphql/mutation/*.ts","./graphql/queries/*.ts"],
   generates: {
-    "src/gql": {
+    "./graphql/codegen/": {
       preset: "client",
       plugins: []
     },
-    "./graphql.schema.json": {
-      plugins: ["introspection"]
-    }
   }
 };
 
