b9d467797005753399a8fb7127978e6f1bb12d91
always-reach <67514209+always-reach@users.noreply.github.com>
2022-10-29 17:04:38 +0900

    fix useReactiveVar

--- front/components/Header.tsx
+++ front/components/Header.tsx
@@ -1,14 +1,11 @@
+import { useReactiveVar } from "@apollo/client"
 import * as React from "react"
 import { isSignInVar } from "../pages/_app"
 
 const Header = () => {
-    console.log("rendering header")
-    console.log(isSignInVar())
-
+    const isSignIn=useReactiveVar(isSignInVar)
     const signout=()=>{
         isSignInVar({})
-        console.log("header signout")
-        console.log(isSignInVar())
     }
     return (
         <nav>
@@ -17,7 +14,7 @@ const Header = () => {
                     <h2 className="text-5xl text-gray-900 font-bold p-5">MURDER MYSTERY</h2>
                 </div>
                 <div className="flex flex-col justify-center p-3">
-                    {isSignInVar().signinUser && <h4 className="text-2xl text-gray-900">
+                    {isSignIn.signinUser && <h4 className="text-2xl text-gray-900">
                         <button onClick={signout}>SIGNOUT</button>
                         </h4>}
                 </div>
