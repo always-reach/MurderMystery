9bd3675811314c5f1c4c02f6395713da59c51c96
always-reach <67514209+always-reach@users.noreply.github.com>
2022-10-29 18:57:11 +0900

    add access control

--- front/pages/_app.tsx
+++ front/pages/_app.tsx
@@ -1,12 +1,16 @@
 import '../styles/globals.css'
 import * as React from 'react'
-import { ApolloClient, InMemoryCache, ApolloProvider, makeVar } from '@apollo/client'
 import { NextPage } from 'next'
-import type { AppProps } from 'next/app'
+import { useRouter } from 'next/router'
+import { ApolloClient, InMemoryCache, ApolloProvider, makeVar, useReactiveVar } from '@apollo/client'
 import { Signin_UserMutation } from '../graphql/codegen'
 
 
-export const isSignInVar=makeVar<Signin_UserMutation>({})
+import type { AppProps } from 'next/app'
+import { accessControl, GetAccessControl, useAccessControl } from '../access_control/AccessControl'
+
+
+export const isSignInVar = makeVar<Signin_UserMutation>({})
 const cache = new InMemoryCache()
 const client = new ApolloClient({
   uri: "http://localhost:8000/graphql",
@@ -15,14 +19,19 @@ const client = new ApolloClient({
 
 export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
   getLayout?: (page: React.ReactElement) => React.ReactNode
+  getAccessControl?: GetAccessControl
 }
+
 
 type AppPropsWithLayout = AppProps & {
   Component: NextPageWithLayout
 }
 
+
 function MyApp({ Component, pageProps }: AppPropsWithLayout) {
   const getlayout = Component.getLayout ?? ((page) => page)
+  const { getAccessControl = accessControl } = Component
+  useAccessControl(getAccessControl)
   return (
     getlayout(
       <ApolloProvider client={client}>
