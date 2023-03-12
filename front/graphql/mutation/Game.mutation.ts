cd260536b0be819e0146fff3815c827a43cc8f77
always-reach <67514209+always-reach@users.noreply.github.com>
2023-03-12 21:00:19 +0900

    gql修正

---
+++ front/graphql/mutation/Game.mutation.ts
@@ -0,0 +1,84 @@
+import { gql } from "@apollo/client";
+
+
+export const CREATE_GAME = gql`
+mutation CREATE_GAME(
+        $title:String!,
+        $auther:String,
+        $playTimeMinute:Int,
+        $maxPlayerCount:Int,
+        $minPlayerCount:Int,
+        $note:String,
+        $image:Upload,
+        $playedAt:Date,
+        $user:Int
+        ){
+    createGame(
+            title:$title,
+            auther:$auther,
+            playTimeMinute:$playTimeMinute,
+            maxPlayerCount:$maxPlayerCount,
+            minPlayerCount:$minPlayerCount,
+            note:$note,
+            image:$image,
+            playedAt:$playedAt,
+            user:$user){
+        game{
+            id
+            title
+            auther
+            playTimeMinute
+            maxPlayerCount
+            minPlayerCount
+            note
+            image
+            playedAt
+            user{
+                id
+            }
+        }
+    }
+}
+`
+
+export const PLAYED_GAME = gql`
+    mutation PLAYED_GAME($userId:Int!,$gameId:Int!){
+        playedGame(userId:$userId,gameId:$gameId){
+            game{
+                id
+                title
+                auther
+                playTimeMinute
+                maxPlayerCount
+                minPlayerCount
+                note
+                image
+                playedAt
+                user{
+                    id
+                }
+            }
+        }
+    }
+`
+
+export const REMOVE_PLAYED_GAME = gql`
+    mutation REMOVE_PLAYED_GAME($userId:Int!,$gameId:Int!){
+        removePlayedGame(userId:$userId,gameId:$gameId){
+            game{
+                id
+                title
+                auther
+                playTimeMinute
+                maxPlayerCount
+                minPlayerCount
+                note
+                image
+                playedAt
+                user{
+                    id
+                }
+            }
+        }
+    }
+`
